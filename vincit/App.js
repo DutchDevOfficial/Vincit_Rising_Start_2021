5c29095b9fce909d1dc5222615f4db0164eb9aa2
Valtteri Hendriksson <n9heva00@students.oamk.fi>
2022-03-25 11:29:05 +0200

    Maximize profits

--- vincit/App.js
+++ vincit/App.js
@@ -1,29 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, Button, Platform, Pressable} from 'react-native';
-import React, { useEffect, useState,  SafeAreaView, TextInput } from "react";
+import { Text, View} from 'react-native';
+import React, { useState } from "react";
 import DatePicker from './components/DatePicker';
 import Chart from './components/Chart';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import styles from './style/style';
-
-
+import MaximizeProfit from './components/MaximizeProfit';
 
 export default function App() {
-  const [data, setData] = useState([]);
 
-const childToParent =  (asd) => {
+  const [data, setData] = useState([]);
+  
+  const childToParent = (asd) => {
     setData(asd.prices);
-}
+  }
 
   return (
     <View>
-      <Header/>
-  <DatePicker childToParent={childToParent}/>
-  <Chart datafromparent={data}/>
-  <Text>This is a test1</Text>
-  <Footer/>
-</View>
+      <Header />
+      <DatePicker childToParent={childToParent} />
+      <Chart datafromparent={data} />
+      <MaximizeProfit data={data} />
+      <Text>This is a test1</Text>
+      <Footer />
+    </View>
   );
 }
 
---
+++ vincit/components/MaximizeProfit.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React, { useState } from 'react'
+
+export default function MaximizeProfit(data) {
+  console.log(data)
+  const [Prices, setPrices] = useState([]);
+  const [MaxDifference, setMaxDifference] = useState([]);
+  const [BuyDate, setBuyDate] = useState([]);
+  const [SellDate, setSellDate] = useState([]);
+
+  return (
+    <View>
+      <Text>Maximize Profit</Text>
+    </View>
+  )
+}
