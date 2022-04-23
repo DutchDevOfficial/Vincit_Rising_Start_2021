2d4185b7ab245b607f1632a30f6f836990ba4292
Haatis <joona00@windowslive.com>
2022-04-23 13:15:02 +0300

    fix crypto

--- components/MaximizeProfit.js
+++ components/MaximizeProfit.js
@@ -35,24 +35,18 @@ export default function MaximizeProfit({ parentData, currency}) {
   }
 
   return (
-<<<<<<< HEAD
     <View style={style.slotContainer}>
       
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
-        <View>
-          <Text style={{width: 130, textAlign: 'center',color: '#fff',fontSize: 17, }}>Maximise profit</Text>
-        </View>
-        <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+    <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
+      <View>
+        <Text style={{width: 130, textAlign: 'center',color: '#fff',fontSize: 17, }}>Maximise profit</Text>
       </View>
-
-      {/* <Text style={style.text}>Maximize Profit</Text> */}
-      <Text style={style.text2}>{text}</Text>
-=======
-    <View style={style.container}>
-      <Text style={style.text2}>Maximize Profit</Text>
-      <Text>{text}</Text>
->>>>>>> e52cdbb45e29bc541ccd7929efbfd828c3b7e5d1
+      <View style={{flex: 1, height: 1, backgroundColor: 'black'}} />
     </View>
+
+    {/* <Text style={style.text}>Maximize Profit</Text> */}
+    <Text style={style.text2}>{text}</Text>
+  </View>
   )
 }
